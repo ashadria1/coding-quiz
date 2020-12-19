31a659806b688ef31abd3be6c024a7f61ba86c41
ashadria <ashadria@gmail.com>
2020-12-18 19:33:49 -0500

    adding code for all but timer

---
+++ questions.js
@@ -0,0 +1,133 @@
+    var questions = [
+        {
+            title: "Inside which HTML element do we put the JavaScript?",
+            choices: ["<javascript>", "<script>", "<js>", "<scripting>"],
+            answer: "<script>",
+        },
+        {
+            title: "JavaScript is a ___ -side programming language.", 
+            choices: ["Client", "Server", "Both", "None"],
+            answer: "Both",
+        },
+        {
+            title: "Where is the correct place to insert a JavaScript?",
+            choices: ["The <head> section", "The <body> section", "Both the <head> section and <body> section are correct.", "After the FORTRAN code"],
+            answer: "Both the <head> section and <body> section are correct.",
+        },
+        {
+            title: "How do you find the minimum of x and y using JavaScript?",
+            choices: ["min(x,y);", "Math.min(x,y);", "Math.min(xy);", "min(xy);"],
+            answer: "Math.min(x,y);",
+        },
+        {
+            title: "The external JavaScript file must contain the <script> tag.",
+            choices: ["True", "False", "Sometimes", "Depends on the browser type"],
+            answer: "False",
+        },
+        {
+            title: "Which JavaScript label catches all the values, except for the ones specified?",
+            choices: ["catch", "label", "try", "default"],
+            answer: "default",
+        },
+        {
+            title: "How do you create a function in JavaScript?",
+            choices: ["function:myFunction()", "function = myFunction()", "function myFunction()", "conjunctionJunction = $whatsYourFunction()"],
+            answer: "function myFunction()",
+        },
+        {
+            title: "How do you call a function named myFunction?",
+            choices: ["call myFunction()", "myFunction()", "call function myFunction()", "summon myFunction()"],
+            answer: "myFunction()",
+        },
+        {
+            title: "How to write an IF statement in JavaScript?",
+            choices: ["if i = 5", "if i = 5 then", "if (i == 5)", "if i == 5 then"],
+            answer: "if (i == 5)",
+        },
+        {
+            title: "How to write an IF statement for executing some code if i is NOT equal to 5?",
+            choices: ["if (i <> 5)", "if i =! 5 then", "if (i != 5)", "if i <> 5"],
+            answer: "if (i != 5)",
+        },
+        {
+            title: "How does a WHILE loop start?",
+            choices: ["while i = 1 to 10", "while (i <= 10)", "while (i <= 10; i++)", "while i = 1-10"],
+            answer: "while (i <= 10)",
+        },
+        {
+            title: "How does a FOR loop start?",
+            choices: ["for (i = 0; i <= 5)", "for (i = 0; i <= 5; i++)", "for i = 1 to 5", "for (i <= 5 ; i++)"],
+            answer: "for (i = 0; i <= 5; i++)",
+        },
+        {
+            title: "How can you add a comment in a JavaScript?",
+            choices: ["//This is a comment", "<!--This is a comment-->", "'This is a comment", "Comments cannot be added in Javascript"],
+            answer: "//This is a comment",
+        },
+        {
+            title: "How to insert a comment that has more than one line?",
+            choices: ["//This comment has <br> more than one line//", "/*This comment has <br> more than one line*/", "<!--This comment has <br> more than one line-->", "Comments can only be on one line"],
+            answer: "/*This comment has <br> more than one line*/",
+        },
+        {
+            title: "Which are the correct if statements to execute certain code if “x” is equal to 2?",
+            choices: ["if(x 2)", "if(x = 2)", "if(x == 2)", "if(x != 2 )"],
+            answer: "if(x == 2)",
+        },
+        {
+            title: "How do you round the number 7.25, to the nearest integer?",
+            choices: ["Math.rnd(7.25)", "round(7.25)", "Math.round(7.25)", "rnd(7.25)"],
+            answer: "Math.round(7.25)",
+        },
+        {
+            title: "How do you find the number with the highest value of x and y?",
+            choices: ["Math.ceil(x,y)", "Math.max(x,y)", "ceil(x,y)", "top(x,y)"],
+            answer: "Math.max(x,y)",
+        },
+        {
+            title: "Which of the following type of variable is visible everywhere in your JavaScript code?",
+            choices: ["global variable", "local variable", "Both of the above", "None of the above"],
+            answer: "global variable",
+        },
+        {
+            title: "JavaScript is the same as Java.",
+            choices: ["True", "False", "Sometimes", "Depends on the operating system"],
+            answer: "False",
+        },
+        {
+            title: "How can you detect the client's browser name?",
+            choices: ["browser.name", "client.navName", "navigator.appName", "local.storage"],
+            answer: "navigator.appName",
+        },
+        {
+            title: "Which event occurs when the user clicks on an HTML element?",
+            choices: ["onward", "onchange", "onclick", "onmouseover"],
+            answer: "onclick",
+        },
+        {
+            title: "How do you declare a JavaScript variable?",
+            choices: ["v carName;", "variable carName;", "var carName;", "var: carName"],
+            answer: "var carName;",
+        },
+        {
+            title: "Which operator is used to assign a value to a variable?",
+            choices: ["*", "=", "x", "-"],
+            answer: "=",
+        },
+        {
+            title: "What will the following code return: Boolean(10 > 9)",
+            choices: ["NaN", "false", "true", "Syntax Error"],
+            answer: "true",
+        },
+        {
+            title: "Is JavaScript case-sensitive?",
+            choices: ["Yes", "No", "depends on the browser", "depends on the operating system"],
+            answer: "Yes",
+        },
+        
+    ];
+
+
+
+    
+
